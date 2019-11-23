f29a6989efb426aae3226479657050758361f654
Cak Suku <ilzammulkhaq85@gmail.com>
2019-11-23 22:08:14 +0700

    Bismillah Belajar Dasar Navigation Jetpack

---
+++ app/src/main/java/com/studio/suku/forecast/ui/setting/SettingFragment.kt
@@ -0,0 +1,10 @@
+package com.studio.suku.forecast.ui.setting
+
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingFragment : Fragment(){
+
+    //TODO : Setting Fragment
+
+}
